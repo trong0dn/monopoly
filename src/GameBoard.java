4830244e66d2ff1eacc22fbb2a55d57150aa3d36
elishajcat <44349103+elishajcat@users.noreply.github.com>
2021-10-17 11:56:11 -0400

    Add files via upload

---
+++ src/GameBoard.java
@@ -0,0 +1,84 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Random;
+
+/**
+ * GameBoard Class
+ */
+public class GameBoard {
+    private HashMap<Integer, Property> propertyLayout;
+    private ArrayList<String> players;
+
+    public GameBoard() {
+        this.propertyLayout = new HashMap<>();
+        this.players = new ArrayList<>();
+    }
+
+    /**
+     * Get the position of the player
+     *
+     * @param player Player
+     * @return int
+     */
+    public int getPosition(Player player){
+        return player.getPosition();
+    }
+
+    /**
+     * Create a new property to be added to propertyLayout
+     *
+     * @param name String
+     * @param rent int
+     * @param price int
+     */
+    private void createProperty(String name, int rent, int price, int position){
+        Property newProperty = new Property(name, rent, price);
+        propertyLayout.put(position, newProperty);
+    }
+
+    /**
+     * Get the number of players
+     *
+     * @return Integer
+     */
+    public Integer getNumPlayers() {
+        return players.size();
+    }
+
+    /**
+     * Remove a player that is bankrupt
+     *
+     * @param playerCharacter String
+     */
+    public void removePlayers(String playerCharacter) {
+        players.remove(playerCharacter);
+    }
+
+    /**
+     * Roll a random number from 1 to 12
+     *
+     * @return int
+     */
+    public int rollDice() {
+        int diceNumber = new Random().nextInt(12) + 1;
+        return diceNumber;
+    }
+
+    /**
+     * Change the player's position
+     *
+     * @param player Player
+     */
+    public void playerMovement(Player player) {
+        int newPosition = (player.getPosition() + rollDice()) % 22;
+
+        player.setPosition(newPosition);
+    }
+
+    /**
+     * The text-based playable version of Monopoly. You can buy and sell properties, pay rent and become bankrupt
+     */
+    public void play(){
+
+    }
+}
