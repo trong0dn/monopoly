3a35582f7f14ff9a13f1b84b2d0419e21152645d
trong0dn <55768917+trong0dn@users.noreply.github.com>
2021-10-17 19:07:33 -0400

    Add files via upload

---
+++ src/RollDice.java
@@ -0,0 +1,35 @@
+import java.util.Random;
+
+/**
+ * Class represents rolling dice.
+ * @author Trong Nguyen
+ */
+public class RollDice implements Dice {
+    private final int NUM_DICE; // number of dice
+    private final int NUM_SIDES; // number of sides per die
+
+    public RollDice() {
+        this.NUM_DICE = 2;
+        this.NUM_SIDES = 6;
+    }
+
+    @Override
+    public int getNumDice() {
+        return this.NUM_DICE;
+    }
+
+    @Override
+    public int getNumSides() {
+        return this.NUM_SIDES;
+    }
+
+    @Override
+    public Roll rollDice() {
+        Roll roll = new Roll();
+        int die1 = new Random().nextInt(this.NUM_SIDES) + 1;
+        int die2 = new Random().nextInt(this.NUM_SIDES) + 1;
+        roll.value = die1 + die2;
+        roll.isDouble = die1 == die2;
+        return roll;
+    }
+}
