cbeb6f3ed1a65d171d72706e1833598218e614b4
trong0dn <55768917+trong0dn@users.noreply.github.com>
2021-10-18 00:36:06 -0400

    Add content to GameBoard.java

--- src/GameBoard.java
+++ src/GameBoard.java
@@ -9,4 +9,38 @@ public class GameBoard {
     public int size() {
         return this.NUM_TILES;
     }
+
+    /**
+     * Get the board.
+     * @return  Square[], representation of the board game
+     */
+    public Square[] getBoard() {
+        return this.board;
+    }
+
+    /**
+     * Get board position from square position.
+     * @param position  int, index of the postion
+     * @return          Square
+     */
+    public Square square(int position) {
+        return board[position];
+    }
+
+    /**
+     * Downcast all square that is a property.
+     * @param name  String, property name
+     * @return      Property
+     */
+    private Property property(String name) {
+        for (Square sq : board) {
+            if (sq instanceof Property && sq.name().equals(name)) {
+                return (Property) sq;
+            }
+        }
+        return null;
+    }
+
+
+
 }
