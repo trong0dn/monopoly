2765a33bacc3d6a381f00de74d483671946ace61
trong0dn <55768917+trong0dn@users.noreply.github.com>
2021-10-18 14:02:31 -0400

    Add content to GameBoard.java

--- src/GameBoard.java
+++ src/GameBoard.java
@@ -41,6 +41,118 @@ public class GameBoard {
         return null;
     }
 
+    /**
+     * Create the square if the position is being occupied.
+     * @param position  int, index of the position
+     * @return          Square
+     */
+    private Square makeSquare(int position) {
+        switch (position) {
+            case 0:
+                return go(position);
+            case 1:
+                return oldKent(position);
+            case 2:
+                return communityChest(position);
+            case 3:
+                return whitechapel(position);
+            case 4:
+                return incomeTax(position);
+            case 5:
+                return kingsCrossStation(position);
+            case 6:
+                return theAngelIslington(position);
+            case 7:
+                return chance(position);
+            case 8:
+                return euston(position);
+            case 9:
+                return pentonville(position);
+            case 10:
+                return justVisiting(position);
+            case 11:
+                return pallMall(position);
+            case 12:
+                return electricCompany(position);
+            case 13:
+                return whitehall(position);
+            case 14:
+                return northumrld(position);
+            case 15:
+                return maryleboneStation(position);
+            case 16:
+                return bow(position);
+            case 17:
+                return communityChest(position);
+            case 18:
+                return marlborough(position);
+            case 19:
+                return vine(position);
+            case 20:
+                return freeParking(position);
+            case 21:
+                return strand(position);
+            case 22:
+                return chance(position);
+            case 23:
+                return fleet(position);
+            case 24:
+                return trafalgar(position);
+            case 25:
+                return fenchurchStStation(position);
+            case 26:
+                return leicester(position);
+            case 27:
+                return conventry(position);
+            case 28:
+                return waterWorks(position);
+            case 29:
+                return piccadilly(position);
+            case 30:
+                return goToJail(position);
+            case 31:
+                return regent(position);
+            case 32:
+                return oxford(position);
+            case 33:
+                return communityChest(position);
+            case 34:
+                return bond(position);
+            case 35:
+                return liverpoolStStation(position);
+            case 36:
+                return chance(position);
+            case 37:
+                return park(position);
+            case 38:
+                return superTax(position);
+            case 39:
+                return mayfair(position);
+            case 40:
+                return inJail(position);
+            default:
+                return null;
+        }
+    }
 
+    public Square go(int position) {
+        return new Inactive(position, "Go");
+    }
+
+    public Square freeParking(int position) {
+        return new Inactive(position, "Free Parking");
+    }
+
+    public Square goToJail(int position) {
+        return new Jail(position, "Go to Jail", Jail.JailType.GOTO_JAIL);
+    }
+
+    public Square inJail(int position) {
+        return new Jail(position, "In Jail", Jail.JailType.IN_JAIL);
+    }
+
+    public Square justVisiting(int position) {
+        return new Jail(position, "Just Visiting", Jail.JailType.JUST_VISITING);
+    }
 
 }
---
+++ src/Jail.java
@@ -0,0 +1,54 @@
+public class Jail implements Square {
+    private final int position;
+    private final JailType type;
+    private final String name;
+
+    public Jail(int position, String name, JailType type) {
+        this.position = position;
+        this.name = name;
+        this.type = type;
+    }
+
+    @Override
+    public int position() {
+        return this.position;
+    }
+
+    @Override
+    public String name() {
+        return this.name;
+    }
+
+    @Override
+    public boolean isOwnable() {
+        return false;
+    }
+
+    @Override
+    public boolean isOwned() {
+        return false;
+    }
+
+    @Override
+    public int cost() {
+        return 0;
+    }
+
+    @Override
+    public void purchase(Player player) {
+    }
+
+    @Override
+    public int rent(int val) {
+        return 0;
+    }
+
+    @Override
+    public Player owner() {
+        return null;
+    }
+
+    public enum JailType {
+        JUST_VISITING, IN_JAIL, GOTO_JAIL
+    }
+}
