b46094720013d7ca200e961b6cccc1f0c8f7b863
trong0dn <55768917+trong0dn@users.noreply.github.com>
2021-10-17 21:00:20 -0400

    Added more content to main of Monopoly class

--- src/Monopoly.java
+++ src/Monopoly.java
@@ -1,6 +1,8 @@
 public class Monopoly {
 
     public static void main(String[] args) {
-
+        RollDice rollDice = new RollDice();
+        System.out.println(rollDice.rollDice().value);
+        System.out.println(rollDice.rollDice().isDouble);
     }
 }
