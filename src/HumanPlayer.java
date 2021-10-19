f2fe3a9995b8efe5aba6eac3f786f7db1e36719f
trong0dn <55768917+trong0dn@users.noreply.github.com>
2021-10-19 13:39:53 -0400

    Added HumanPlayer

--- src/HumanPlayer.java
+++ src/HumanPlayer.java
@@ -1,7 +1,6 @@
 import java.util.*;
 
 public class HumanPlayer implements Player {
-    private final int BOARD_SIZE = 40;
     private final Queue<Square> properties;
     private final String playerName;
     private int money;
@@ -9,7 +8,7 @@ public class HumanPlayer implements Player {
 
 
     public HumanPlayer(String playerName) {
-        this.properties = new LinkedList<Square>();
+        this.properties = new LinkedList<>();
         this.playerName = playerName;
         this.money = 1500;
         this.position = 0;
@@ -18,48 +17,58 @@ public class HumanPlayer implements Player {
     @Override
     public void move(int numTiles) {
         this.position += numTiles;
+        int BOARD_SIZE = 40;
         if (position >= BOARD_SIZE) {
-
+            position -= BOARD_SIZE;
+            collectMoney(200);
         }
     }
 
     @Override
-    public void moveTo(int position) {
-
+    public void moveTo(int newPosition) {
+        if (newPosition < position) {
+            collectMoney(200);
+        }
+        position = newPosition;
     }
 
     @Override
     public int getPosition() {
-        return 0;
+        return this.position;
     }
 
     @Override
-    public Iterable<Square> properties() {
-        return null;
+    public Queue<Square> properties() {
+        return new LinkedList<>(properties);
     }
 
     @Override
     public String name() {
-        return null;
+        return this.playerName;
     }
 
     @Override
     public int getMoney() {
-        return 0;
+        return this.money;
     }
 
     @Override
     public void collectMoney(int money) {
-
+        this.money += money;
     }
 
     @Override
     public void buyProperty(Square square) {
-
+        if (!square.isOwnable()) {
+            throw new IllegalArgumentException("This property can not be purchased");
+        } else {
+            properties.add(square);
+            square.purchase(this);
+        }
     }
 
     @Override
     public void sellProperty(Square square) {
-
+        properties.remove(square);
     }
 }
