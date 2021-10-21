4f7d6be7f1a9a61862ef356e145bab881b33bae2
trong0dn <55768917+trong0dn@users.noreply.github.com>
2021-10-21 16:38:47 -0400

    Added play() method

--- src/Monopoly.java
+++ src/Monopoly.java
@@ -1,19 +1,20 @@
-import java.util.ArrayList;
-import java.util.Queue;
-import java.util.Scanner;
+import java.util.*;
 
 public class Monopoly {
-    private final RollDice rollDice;
-    private final DecisionState decisionState;
-    private final ArrayList<Player> playerList;
-    private final boolean success;
+    private RollDice rollDice;
+    private GameState gameState;
+    private boolean isBankrupt;
 
 
-    public Monopoly(boolean success) {
-        playerList = new ArrayList<>();
-        this.success = success;
-        rollDice = new RollDice();
-        decisionState = DecisionState.NONE;
+    public Monopoly() {
+        this.rollDice = new RollDice();
+        this.isBankrupt = false;
+        this.gameState = new GameState();
+        gameState.players = new LinkedList<>();
+        gameState.decisionState = DecisionState.NONE;
+        gameState.gameBoard = new GameBoard();
+        gameState.currentPlayer = null;
+        initialize();
     }
 
     /**
@@ -34,89 +35,39 @@ public class Monopoly {
         public int counter = 0;
     }
 
-
-    /**
-     * @return the number of player
-     * adds new players to a list.
-     */
-    public int numOfPlayers (){
-        /*do {
-            System.out.print("How many players would like to play (min 2): ");
+    public void play() {
+        while (gameState.players.size() > 1) {
             try {
-                num = input.nextInt();
-                success = true;
-            } catch (Exception e) {
-                System.out.println("Wrong input type (only integers)");
-                success = false;
-                input.next();
+                gameState.currentPlayer = gameState.players.remove();
+                turn();
+                if (!isBankrupt) {
+                    gameState.players.add(gameState.currentPlayer);
+                }
+                isBankrupt = false;
+            } catch (NoSuchElementException e) {
+                System.err.println("Game failed to be initialized");
+                return;
+            } finally {
+                printState();
             }
-        }while (!success);*/
-
-        Scanner input = new Scanner(System.in);
-        System.out.print("How many players would like to play (min 2, max 8): ");
-        int num = input.nextInt();
-
-        while (num < 2 || num > 8) {
-            System.out.println("Try Again! You must have a min of 2 and max of 8 players: ");
-            num = input.nextInt();
-        }
-       // System.out.println("there are: " + num + " players");
-        String newPlayerName;
-        input.nextLine();
-        for (int i =1; i<num+1; i++){
-            System.out.print("Player #" + i + ", enter your username: ");
-            newPlayerName = input.nextLine();
-            HumanPlayer newPlayer = new HumanPlayer(newPlayerName);
-            playerList.add(newPlayer);
-
-        }
-        System.out.println("\nthere are: " + num+" players, with the usernames: ");
-        for (Player p: playerList){
-            System.out.println(p.name());
-        }
-
-        return num;
-    }
-
-
-    /**
-     * @param player    Player
-     * @param square    Square
-     */
-    public void handleSquare(Player player, Square square){
-        //another player owns this, you must pay rent
-        if (square.isOwned()){
-            player.payRent(square);
-            //make player that owns it receive rent
-        }
-        else if (square.isOwnable()){
-            player.buyProperty(square);
         }
     }
 
+    public void initialize() {
 
-    /**
-     * pay rent
-     * buy houses
-     */
-    public void handleSquare(){
-        //
     }
 
+    public void turn() {
 
+    }
 
+    public void printState() {
 
-
+    }
 
     public static void main(String[] args) {
-        RollDice rollDice = new RollDice();
-        System.out.println(rollDice.rollDice().value);
-        System.out.println(rollDice.rollDice().isDouble);
-
-        Monopoly m = new Monopoly(true);
-        m.numOfPlayers();
-
-
+        Monopoly monopoly = new Monopoly();
+        monopoly.play();
 
     }
 }
