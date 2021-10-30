4d6a43a340f88deb3b8eb29e5b679bce8a7413b4
trong0dn <55768917+trong0dn@users.noreply.github.com>
2021-10-30 13:13:09 -0400

    Fixed up the images

--- src/MonopolyFrame.java
+++ src/MonopolyFrame.java
@@ -26,14 +26,13 @@ public class MonopolyFrame extends JFrame {
             String s = Integer.toString(i);
             BufferedImage image = null;
             try {
-                image = ImageIO.read(Objects.requireNonNull(getClass().getResource("/images/" + s + ".png")));
+                image = ImageIO.read(Objects.requireNonNull(getClass().getResource("/images/" + s + ".jpg")));
             } catch (IOException e) {
                 e.printStackTrace();
             }
             assert image != null;
-            int imageWidth = (int) (image.getWidth() * 0.5);
-            int imageHeight = (int) (image.getHeight() * 0.5);
-            System.out.println(imageHeight);
+            int imageWidth = (int) (image.getWidth() * 0.15);
+            int imageHeight = (int) (image.getHeight() * 0.15);
             Image scaledImage = image.getScaledInstance(imageWidth, imageHeight, Image.SCALE_DEFAULT);
             JLabel imageLabel = new JLabel(new ImageIcon(scaledImage));
             imageSquareList.add(imageLabel);
