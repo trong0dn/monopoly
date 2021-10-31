cfd54076880e7698eb456636d2a9b2801baf28e0
trong0dn <55768917+trong0dn@users.noreply.github.com>
2021-10-31 12:14:49 -0400

    Created new GUI classes

--- src/GameBoardGUI.java
+++ src/GameBoardGUI.java
@@ -1,6 +1,7 @@
 import javax.swing.*;
 import javax.swing.border.LineBorder;
 import java.awt.*;
+import java.awt.geom.AffineTransform;
 import java.util.ArrayList;
 
 public class GameBoardGUI extends JPanel {
@@ -12,39 +13,91 @@ public class GameBoardGUI extends JPanel {
     public GameBoardGUI(int x, int y, int width, int height) {
         this.setBorder(new LineBorder(Color.BLACK));
         this.setBounds(x, y, width, height);
+        this.gameBoard = new GameBoard();
+        this.setLayout(null);
         initializeSquarePanels(width, height);
     }
 
     private void initializeSquarePanels(int width, int height) {
-        int count = 0;
+        ArrayList<String> squareNames = new ArrayList<>();
         for (Square sq : gameBoard.getBoard()) {
-            count++;
-            // Squares on the bottom
-            if (count <= 10) {
-                int x = 10-count;
-                int y = 10;
-                SquareGUI squareGUI = new SquareGUI(x, y, WIDTH, HEIGHT, sq.name(), 0);
-                squareGUIs.add(squareGUI);
-            }
-            // Squares on the left
-            else if (count <= 20) {
-                int x = 0;
-                int y = 20-count;
-                SquareGUI squareGUI = new SquareGUI(x, y, WIDTH, HEIGHT, sq.name(), 90);
-            }
-            // Squares on the top
-            else if (count <= 30) {
-                int x = count-20;
-                int y = 0;
-                SquareGUI squareGUI = new SquareGUI(x, y, WIDTH, HEIGHT, sq.name(), 180);
-            }
-            // Squares on the right
-            else if (count < 40){
-                int x = 10;
-                int y = count- 30;
-                SquareGUI squareGUI = new SquareGUI(x, y, WIDTH, HEIGHT, sq.name(), -90);
-            }
+            squareNames.add(sq.name());
         }
+
+        SquareGUI square;
+        int xOffset = 5;
+        int yOffset = 5;
+        int maxX = 550;
+        int maxY = 550;
+        int x = xOffset + maxX;
+        int y = yOffset + maxY;
+
+        // Create right-bottom corner square: "GO"
+        square = new SquareGUI(x, y,100,100, squareNames.get(0),-45);
+        this.add(square);
+
+        // Create bottom squares
+        for (int i = 0; i < 9; i++) {
+            x -= 50;
+            square = new SquareGUI(x, y,50,100, squareNames.get(i+1),0);
+            this.add(square);
+        }
+
+        // Create left-bottom corner square: "JAIL"
+        square = new SquareGUI(xOffset, y,100,100, squareNames.get(10),45);
+        this.add(square);
+
+        // Create left squares
+        for (int i = 0; i < 9; i++) {
+            y -= 50;
+            square = new SquareGUI(xOffset, y,100,50, squareNames.get(i+11),90);
+            this.add(square);
+        }
+
+        // Create top-left corner square: "FREE PARKING"
+        square = new SquareGUI(xOffset, yOffset,100,100, squareNames.get(20),135);
+        this.add(square);
+
+        // Create top squares
+        for (int i = 0; i < 9; i++) {
+            square = new SquareGUI(x, yOffset,50,100, squareNames.get(i+21),180);
+            x += 50;
+            this.add(square);
+        }
+
+        // Create top-right corner square: "GO TO JAIL"
+        square = new SquareGUI(555,5,100,100, squareNames.get(30),-135);
+        this.add(square);
+
+        // Create right squares
+        for (int i = 0; i < 9; i++) {
+            square = new SquareGUI(x, y,100,50, squareNames.get(i+31),-90);
+            y += 50;
+            this.add(square);
+        }
+
+        // Create center of the game board
+        JLabel centerLabel = new JLabel("MONOPOLY"){
+            protected void paintComponent(Graphics g) {
+                Graphics2D g2 = (Graphics2D)g;
+                g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+                AffineTransform aT = g2.getTransform();
+                Shape oldShape = g2.getClip();
+                double x = getWidth()/2.0;
+                double y = getHeight()/2.0;
+                aT.rotate(Math.toRadians(-45), x, y);
+                g2.setTransform(aT);
+                g2.setClip(oldShape);
+                super.paintComponent(g);
+            }
+        };
+        centerLabel.setForeground(Color.WHITE);
+        centerLabel.setBackground(Color.RED);
+        centerLabel.setOpaque(true);
+        centerLabel.setHorizontalAlignment(SwingConstants.CENTER);
+        centerLabel.setFont(new Font("Lucida Grande", Font.PLAIN, 40));
+        centerLabel.setBounds(200, 280, 263, 55);
+        this.add(centerLabel);
     }
 
     public void paintComponent(Graphics g) {
--- src/MonopolyGUI.java
+++ src/MonopolyGUI.java
@@ -1,4 +1,33 @@
 import javax.swing.*;
+import javax.swing.border.EmptyBorder;
+import javax.swing.border.LineBorder;
+import java.awt.*;
 
-public class MonopolyGUI  extends JFrame {
+public class MonopolyGUI extends JFrame {
+    private GameBoardGUI gameBoard;
+
+    public MonopolyGUI() {
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setBounds(100, 100, 450, 300);
+        setSize(1080,860);
+        JPanel boxPanel = new JPanel();
+        boxPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
+        setContentPane(boxPanel);
+        boxPanel.setLayout(null);
+
+        JLayeredPane layeredPane = new JLayeredPane();
+        layeredPane.setBorder(new LineBorder(new Color(0, 0, 0)));
+        layeredPane.setBounds(5, 5, 650+20, 650+20);
+        boxPanel.add(layeredPane);
+
+        gameBoard = new GameBoardGUI(5,5,650+10,650+10);
+        gameBoard.setBackground(new Color(51, 255, 153));
+        layeredPane.add(gameBoard, String.valueOf(0));
+
+    }
+
+    public static void main(String[] args) {
+        MonopolyGUI frame = new MonopolyGUI();
+        frame.setVisible(true);
+    }
 }
