7d8a132bebeb1924031bcaa106e02e1cdc9f03dc
Eebro <2ibrahimalmalki@gmail.com>
2021-11-06 14:01:56 -0400

    added PropertyTest file

---
+++ src/PropertyTest.java
@@ -0,0 +1,66 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class PropertyTest {
+
+    @Test
+    public void position() {
+    }
+
+    @Test
+    public void name() {
+    }
+
+    @Test
+    public void isOwnable() {
+    }
+
+    @Test
+    public void isOwned() {
+    }
+
+    @Test
+    public void cost() {
+    }
+
+    @Test
+    public void purchase() {
+    }
+
+    @Test
+    public void rent() {
+    }
+
+    @Test
+    public void owner() {
+    }
+
+    @Test
+    public void isMonopoly() {
+    }
+
+    @Test
+    public void setMonopoly() {
+    }
+
+    @Test
+    public void breakMonopoly() {
+    }
+
+    @Test
+    public void updateMonopoly() {
+    }
+
+    @Test
+    public void getHouseCost() {
+    }
+
+    @Test
+    public void setGroup() {
+    }
+
+    @Test
+    public void testSetGroup() {
+    }
+}
