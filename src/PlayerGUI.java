0cec3a723c36e9bbb212f69e7d14b6e6c64e122f
franciscodegrano <franciscodgrano@gmail.com>
2021-11-06 22:34:43 -0400

    PlayerGUI.java.java javaDoc

--- src/PlayerGUI.java
+++ src/PlayerGUI.java
@@ -2,22 +2,36 @@ import javax.swing.*;
 import java.awt.*;
 import java.util.Collection;
 
+/**
+ * GUI for Player in Game
+ */
 public class PlayerGUI extends JPanel {
     private HumanPlayer player;
     private final int playerNumber;
     static int totalPlayers = 0;
     private int currentSquareNumber = 0;
 
+    // Potential X positions on the board for the player
     private final int[] xLocationsOfPlayer = {550, 495, 445, 395, 345, 295, 245, 195, 145, 95, 0,
             0, 0, 0, 0, 0, 0, 0, 0, 0,
             0, 95, 145, 195, 245, 295, 345, 395, 445, 495, 550,
             550, 550, 550, 550, 550, 550, 550, 550, 550};
 
+    // Potential Y positions on the board for the player
     private final int[] yLocationsOfPlayer = {620, 620, 620, 620, 620, 620, 620, 620, 620, 620, 620,
             525, 475, 425, 375, 325, 275, 225, 175, 125,
             20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20,
             125, 175, 225, 275, 325, 375, 425, 475, 525};
 
+    /*
+    Each X Position has a Y Position, (550, 620)
+     */
+
+    /**
+     * Display Player, Color + Name
+     * @param color, color
+     * @param playerName , String
+     */
     public PlayerGUI(Color color, String playerName) {
         player = new HumanPlayer(playerName);
         totalPlayers++;
@@ -30,6 +44,10 @@ public class PlayerGUI extends JPanel {
         this.setBounds((15+this.playerNumber*5+xLocationsOfPlayer[0]), yLocationsOfPlayer[0], 18, 28);
     }
 
+    /**
+     * Move function
+     * @param diceValue, int
+     */
     public void move(int diceValue) {
         GameBoard gameBoard = new GameBoard();
         int size = gameBoard.size()-1;
