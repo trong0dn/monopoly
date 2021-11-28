5eb76025b577858e5b20f61b49265ec8b61e16aa
elisha <elisha123jc@gmail.com>
2021-11-27 23:58:44 -0500

    Changed UI

--- src/monopoly17/MonopolyGUI.java
+++ src/monopoly17/MonopolyGUI.java
@@ -524,6 +524,8 @@ public class MonopolyGUI extends JPanel {
             buttonBuy.setEnabled(false);
             buttonPayBail.setEnabled(false);
             buttonNextTurn.setEnabled(true);
+
+            updatePlayerStatusTextArea();
         });
 
         return buttonPayBail;
