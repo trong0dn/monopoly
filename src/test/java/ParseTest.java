c61d57f90b8c59376d9974f147194ad6021dbe18
franciscodegrano <franciscodgrano@gmail.com>
2021-12-05 17:43:19 -0500

    Implement Test

--- src/test/java/ParseTest.java
+++ src/test/java/ParseTest.java
@@ -1,8 +1,11 @@
-import monopoly17.JsonParse;
-import monopoly17.GameBoard;
+import monopoly17.*;
+
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
+
+import java.util.Objects;
 
 import static org.junit.Assert.*;
 /*
@@ -12,19 +15,50 @@ import static org.junit.Assert.*;
 /**
  * Test class for parsing for International Languages.
  *
- * @author Francisco De Grano & Ibrahim Alma
+ * @author Francisco De Grano & Ibrahim Almalki
  * Modified By...
  */
 public class ParseTest {
+    // For Sq.1
+    int rent = 2;
+    int oneHouse = 10;
+    int twoHouse = 30;
+    int threeHouse = 90;
+    int fourHouse = 160;
+    int hotel = 250;
+    int propertyCost = 60;
+    int houses = 50;
+
+    Versions versions = Versions.US;
+
+    Property property;
+    Inactive inactive;
+    Taxes taxes;
 
 
-    @Test
-    public void USVersion(){
 
+    @Before
+    public void init(){
+        //Property Sq.1
+        property = new Property(SquareInfo.SQUARE_1.getPosition(),JsonParse.parseJSON(1, String.valueOf(this.versions)),
+                rent, oneHouse, twoHouse, threeHouse, fourHouse, hotel, propertyCost, houses);
+
+        //Inactive Community
+        inactive = new Inactive(SquareInfo.SQUARE_2.getPosition(), JsonParse.parseJSON(2, String.valueOf(this.versions)));
+
+        //Taxes
+        taxes = new Taxes(SquareInfo.SQUARE_4.getPosition(),
+                Objects.requireNonNull(JsonParse.parseJSON(4, String.valueOf(this.versions))));
     }
 
     @Test
-    public void UKVersion(){
+    public void testName(){
+        Assert.assertEquals("MEDITERRANEAN AVENUE", JsonParse.parseJSON(1, String.valueOf(this.versions)));
+        Assert.assertEquals("COMMUNITY CHEST", JsonParse.parseJSON(2, String.valueOf(this.versions)));
+        Assert.ass
 
     }
+
+
+
 }
