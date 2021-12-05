3fc2caa400704e51d078f750325929fdc4513bb7
franciscodegrano <franciscodgrano@gmail.com>
2021-12-05 18:02:30 -0500

    Rail, jail, util

--- src/test/java/ParseTest.java
+++ src/test/java/ParseTest.java
@@ -1,6 +1,5 @@
 import monopoly17.*;
 
-
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
@@ -34,28 +33,42 @@ public class ParseTest {
     Property property;
     Inactive inactive;
     Taxes taxes;
-
-
+    Railroad railroad;
+    Jail jail;
+    Utility utility;
 
     @Before
     public void init(){
-        //Property Sq.1
+        // Property Sq.1 (Property)
         property = new Property(SquareInfo.SQUARE_1.getPosition(),JsonParse.parseJSON(1, String.valueOf(this.versions)),
                 rent, oneHouse, twoHouse, threeHouse, fourHouse, hotel, propertyCost, houses);
 
-        //Inactive Community
+        // Community Sq.2 (Inactive)
         inactive = new Inactive(SquareInfo.SQUARE_2.getPosition(), JsonParse.parseJSON(2, String.valueOf(this.versions)));
 
-        //Taxes
+        // Income Tax Sq.4 (TAXES)
         taxes = new Taxes(SquareInfo.SQUARE_4.getPosition(),
                 Objects.requireNonNull(JsonParse.parseJSON(4, String.valueOf(this.versions))));
+
+        // Railroad
+        railroad = new Railroad(SquareInfo.SQUARE_5.getPosition(), JsonParse.parseJSON(5, String.valueOf(this.versions)));
+
+        // Jail
+        jail = new Jail(SquareInfo.SQUARE_10.getPosition(), JsonParse.parseJSON(10, String.valueOf(this.versions)), Jail.JailType.JUST_VISITING);
+
+        //Utility
+        utility = new Utility(SquareInfo.SQUARE_12.getPosition(), JsonParse.parseJSON(12, String.valueOf(this.versions)));
     }
 
     @Test
     public void testName(){
+
         Assert.assertEquals("MEDITERRANEAN AVENUE", JsonParse.parseJSON(1, String.valueOf(this.versions)));
         Assert.assertEquals("COMMUNITY CHEST", JsonParse.parseJSON(2, String.valueOf(this.versions)));
-        Assert.ass
+        Assert.assertEquals("INCOME TAX", JsonParse.parseJSON(4, String.valueOf(this.versions)));
+        Assert.assertEquals("READING RAILROAD", JsonParse.parseJSON(5, String.valueOf(this.versions)));
+        Assert.assertEquals("JUST VISITING", JsonParse.parseJSON(10, String.valueOf(this.versions)));
+        Assert.assertEquals("ELECTRIC COMPANY", JsonParse.parseJSON(12, String.valueOf(this.versions)));
 
     }
 
