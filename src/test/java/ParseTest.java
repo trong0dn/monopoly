d944698bd21e0fc03429971d42f62e6d4f5c12e0
franciscodegrano <franciscodgrano@gmail.com>
2021-12-05 13:59:24 -0500

    useful link for testing

--- src/test/java/ParseTest.java
+++ src/test/java/ParseTest.java
@@ -1,12 +1,18 @@
+import monopoly17.JsonParse;
+import monopoly17.GameBoard;
+
 import org.junit.Assert;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
+/*
+    helpful link https://mkyong.com/java/jsonassert-how-to-unit-test-json-data/
+ */
 
 /**
  * Test class for parsing for International Languages.
  *
- * @author Francisco De Grano
+ * @author Francisco De Grano & Ibrahim Alma
  * Modified By...
  */
 public class ParseTest {
