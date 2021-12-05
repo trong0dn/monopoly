91a79b8e23cc05695f5c1f82f968fdace2c4a0b8
franciscodegrano <franciscodgrano@gmail.com>
2021-12-05 01:44:43 -0500

    ParseTest implement

---
+++ src/test/java/ParseTest.java
@@ -0,0 +1,24 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Test class for parsing for International Languages.
+ *
+ * @author Francisco De Grano
+ * Modified By...
+ */
+public class ParseTest {
+
+
+    @Test
+    public void USVersion(){
+
+    }
+
+    @Test
+    public void UKVersion(){
+
+    }
+}
