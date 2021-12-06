e6b1da1e0e9fc50c1f1c41857645c1356f95466a
Eebro <2ibrahimalmalki@gmail.com>
2021-12-05 22:04:29 -0500

    added comments

--- src/main/java/monopoly17/JsonParse.java
+++ src/main/java/monopoly17/JsonParse.java
@@ -12,18 +12,18 @@ import java.util.Map;
 public class JsonParse {
 
     /**
-     * @param position the key, position of th esquare
+     * @param position the key, position of the square
      * @param version the language wanted
-     * @return
+     * @return String, the name of the property
      */
     public static String parseJSON(int  position, String version) {
         String pos = String.valueOf(position);
         try {
             // create object mapper instance
-
             ObjectMapper mapper = new ObjectMapper();
 
             // convert JSON file to map
+            //change file path depending on version
             Map<?, ?> map = mapper.readValue(Paths.get("src/main/java/versions/"+version+".json").toFile(), Map.class);
 
             return (String) map.get(pos);
